92c34e9133119692187540214211470ec6a931fc
Tymoteusz Wolodzko <twolodzko+kernelboot@gmail.com>
2018-07-04 10:30:02 +0000

    version 0.1.5

--- R/kernelboot.R
+++ R/kernelboot.R
@@ -32,7 +32,7 @@
 #'                   "multivariate" kernel.
 #' @param ignore     vector of names of columns to be ignored during the smoothing phase of
 #'                   bootstrap procedure (their values are not altered using random noise).
-#' @param parallel   if \code{TRUE}, parallel computing is used (see \code{\link[future]{future_lapply}}).
+#' @param parallel   if \code{TRUE}, parallel computing is used (see \code{\link[future.apply]{future_lapply}}).
 #'                   \emph{Warning:} using parallel computing does not necessary have to
 #'                   lead to improved performance.
 #' @param workers    the number of workers used for parallel computing (see \code{\link[future]{multiprocess}}).
@@ -120,7 +120,7 @@
 #' so that bandwidth parameter controls their standard deviations.
 #'
 #' Random generation from Epanchenikov kernel is done using algorithm
-#' described by Devoye (1986). For optcosine kernel inverse transform
+#' described by Devroye (1986). For optcosine kernel inverse transform
 #' sampling is used. For biweight kernel random values are drawn from
 #' \eqn{\mathrm{Beta}(3, 3)}{Beta(3, 3)} distribution and
 #' \eqn{\mathrm{Beta}(3.3575, 3.3575)}{Beta(3.3575, 3.3575)}
